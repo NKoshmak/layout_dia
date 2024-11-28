f946a500d9f243d8e71dedb7f5198958bf8f80c9
Koshmak Nataliia <koshmaknatali@gmail.com>
2024-11-27 20:07:18 -0700

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+const slides = document.querySelector('.slides');
+const slide = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.arrow-left');
+const nextButton = document.querySelector('.arrow-right');
+
+let currentIndex = 0;
+
+function updateSlide(index) {
+  const slideWidth = slide[0].clientWidth;
+  slides.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex === 0) ? slide.length - 1 : currentIndex - 1;
+  updateSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex === slide.length - 1) ? 0 : currentIndex + 1;
+  updateSlide(currentIndex);
+});
