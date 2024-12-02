fa5bf588dbb7cd6084afda160942fa3d2e0b8d59
Koshmak Nataliia <koshmaknatali@gmail.com>
2024-12-01 18:57:08 -0700

    fixed code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
 'use strict';
-const slides = document.querySelector('.slides');
-const slide = document.querySelectorAll('.slide');
-const prevButton = document.querySelector('.arrow-left');
-const nextButton = document.querySelector('.arrow-right');
+const slides = document.querySelector('.header__slides');
+const slide = document.querySelectorAll('.header__slide');
+const prevButton = document.querySelector('.header__arrow-left');
+const nextButton = document.querySelector('.header__arrow-right');
 
 let currentIndex = 0;
 
@@ -20,3 +20,22 @@ nextButton.addEventListener('click', () => {
   currentIndex = (currentIndex === slide.length - 1) ? 0 : currentIndex + 1;
   updateSlide(currentIndex);
 });
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
